cf420e24687298bdf0d246d1e9271dd050445018
gitvvr <99636484+gitvvr@users.noreply.github.com>
2022-02-27 00:05:08 +0530

    Add files via upload

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,57 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+
+
+apt_update 'update'  do
+    action :update
+end
+
+#include_recipe 'mytomcat::install'
+
+apt_update "update" do
+    action :update
+end
+
+apt_package "tomcat7" do
+ action :install
+end
+
+
+#include_recipe 'mytomcat::configure'
+
+cookbook_file '/etc/default/tomcat7' do
+    source 'tomcat7'
+    action :create
+ #   notifies :restart, 'service[tomcat7]'
+end
+
+service 'tomcat7' do
+    action :restart
+end
+
+
+#include_recipe 'mytomcat::additional'
+package_names = ["tomcat7-docs", "tomcat7-admin" , "tomcat7-examples"]
+
+package_names.each do |package_name| 
+ package package_name do
+     action :install
+ end
+end
+
+#service 'tomcat7' do
+#    action :restart
+#end
+
+cookbook_file '/etc/tomcat7/tomcat-users.xml' do
+ source 'tomcat-users.xml'
+ action :create
+ notifies :restart, 'service[tomcat7]'
+end
+
+
+#service 'tomcat7' do
+#    action :restart
+#end
+ 
